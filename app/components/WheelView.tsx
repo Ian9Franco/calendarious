bc3b74870bfe518fed066fdc98e04af7131b0b55
Notorious <100317984+Ian9Franco@users.noreply.github.com>
2024-10-03 15:05:55 -0300

    arreglando el bug del modo rueda

--- app/components/WheelView.tsx
+++ app/components/WheelView.tsx
@@ -16,22 +16,19 @@ const WheelView: React.FC<WheelViewProps> = ({
   setHoveredSubscription,
   hoveredSubscription,
 }) => {
-  // Calcular el monto total de todas las suscripciones
   const totalAmount = subscriptions.reduce((sum, sub) => sum + sub.amount, 0)
   let startAngle = 0
 
-  // Definir el radio del círculo interno para el monto mensual
-  const innerCircleRadius = 150
+  const outerCircleRadius = 270  // Radio del círculo de los arcos
 
   return (
     <div className="relative h-[300px] sm:h-[600px] w-full">
-      {/* SVG para dibujar el círculo y los arcos de las suscripciones */}
+      {/* SVG para los arcos */}
       <svg width="100%" height="100%" viewBox="0 0 600 600">
-        {/* Círculo base (ahora transparente) */}
         <motion.circle 
           cx="300" 
           cy="300" 
-          r="250" 
+          r={outerCircleRadius} 
           fill="none" 
           stroke="transparent" 
           strokeWidth="30"
@@ -39,32 +36,25 @@ const WheelView: React.FC<WheelViewProps> = ({
           animate={{ pathLength: 1 }}
           transition={{ duration: 1, ease: "easeInOut" }}
         />
-        {/* Mapear las suscripciones para crear los arcos */}
         {subscriptions.map((sub, index) => {
           const angle = (sub.amount / totalAmount) * 360
           const endAngle = startAngle + angle
           const largeArcFlag = angle > 180 ? 1 : 0
-
-          // Añadir un espacio más grande entre las suscripciones
-          const gapAngle = 10 // Aumentado de 2 a 10 grados
+          const gapAngle = 8
           const adjustedStartAngle = startAngle + gapAngle / 2
           const adjustedEndAngle = endAngle - gapAngle / 2
-
-          const startX = 300 + 250 * Math.cos((adjustedStartAngle * Math.PI) / 180)
-          const startY = 300 + 250 * Math.sin((adjustedStartAngle * Math.PI) / 180)
-          const endX = 300 + 250 * Math.cos((adjustedEndAngle * Math.PI) / 180)
-          const endY = 300 + 250 * Math.sin((adjustedEndAngle * Math.PI) / 180)
-
-          const path = `M ${startX} ${startY} A 250 250 0 ${largeArcFlag} 1 ${endX} ${endY}`
-
-          const iconX = 300 + 290 * Math.cos(((adjustedStartAngle + adjustedEndAngle) / 2) * (Math.PI / 180))
-          const iconY = 300 + 290 * Math.sin(((adjustedStartAngle + adjustedEndAngle) / 2) * (Math.PI / 180))
+          const startX = 300 + outerCircleRadius * Math.cos((adjustedStartAngle * Math.PI) / 180)
+          const startY = 300 + outerCircleRadius * Math.sin((adjustedStartAngle * Math.PI) / 180)
+          const endX = 300 + outerCircleRadius * Math.cos((adjustedEndAngle * Math.PI) / 180)
+          const endY = 300 + outerCircleRadius * Math.sin((adjustedEndAngle * Math.PI) / 180)
+          const path = `M ${startX} ${startY} A ${outerCircleRadius} ${outerCircleRadius} 0 ${largeArcFlag} 1 ${endX} ${endY}`
+          const iconX = 300 + 320 * Math.cos(((adjustedStartAngle + adjustedEndAngle) / 2) * (Math.PI / 180))
+          const iconY = 300 + 320 * Math.sin(((adjustedStartAngle + adjustedEndAngle) / 2) * (Math.PI / 180))
 
           startAngle = endAngle
 
           return (
             <g key={sub.name} onMouseEnter={() => setHoveredSubscription(sub)} onMouseLeave={() => setHoveredSubscription(null)}>
-              {/* Arco de la suscripción */}
               <motion.path 
                 d={path} 
                 fill="none" 
@@ -75,7 +65,6 @@ const WheelView: React.FC<WheelViewProps> = ({
                 animate={{ pathLength: 1 }}
                 transition={{ duration: 1, delay: index * 0.1 }}
               />
-              {/* Icono de la suscripción */}
               <motion.foreignObject 
                 x={iconX - 15} 
                 y={iconY - 15} 
@@ -94,51 +83,35 @@ const WheelView: React.FC<WheelViewProps> = ({
             </g>
           )
         })}
-        {/* Círculo interno para el monto mensual */}
-        <circle
-          cx="300"
-          cy="300"
-          r={innerCircleRadius}
-          fill="rgba(0, 0, 0, 0.7)"
-        />
       </svg>
-      {/* Contenedor del gasto mensual total */}
-      <motion.div 
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center flex items-center justify-center"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 1 }}
-        style={{
-          width: `${innerCircleRadius * 2}px`,
-          height: `${innerCircleRadius * 2}px`,
-          flexDirection: 'column',
-        }}
-        >
-        <p className={`${ibmPlexMonoThin.className} text-sm`}>Gasto mensual</p>
-        <p className={`${ibmPlexMonoBold.className} text-2xl`}>${monthlySpend.toFixed(2)}</p>
+
+      {/* Información de la suscripción o valor mensual al pasar el ratón */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-80 p-4 rounded-lg shadow-lg"
+        style={{ border: hoveredSubscription ? `2px solid ${hoveredSubscription.color}` : '2px solid rgba(255, 255, 255, 0.1)' }}
+      >
+        {hoveredSubscription ? (
+          <>
+            <div className="flex items-center mb-2">
+              <IconContext.Provider value={{ size: '2em', color: hoveredSubscription.color }}>
+                <hoveredSubscription.icon />
+              </IconContext.Provider>
+              <span className={`ml-2 text-lg ${ibmPlexMonoBold.className}`}>{hoveredSubscription.name}</span>
+            </div>
+            <p className={`${ibmPlexMonoRegular.className}`}>
+              ${hoveredSubscription.amount.toFixed(2)} / {hoveredSubscription.frequency}
+            </p>
+          </>
+        ) : (
+          <>
+            <p className={`${ibmPlexMonoThin.className} text-sm text-white`}>Gasto mensual total</p>
+            <p className={`${ibmPlexMonoBold.className} text-2xl text-white`}>${monthlySpend.toFixed(2)}</p>
+          </>
+        )}
       </motion.div>
-
-
-      {/* Información de la suscripción al pasar el ratón */}
-      {hoveredSubscription && (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-80 p-4 rounded-lg shadow-lg"
-          style={{ border: `2px solid ${hoveredSubscription.color}` }}
-        >
-          <div className="flex items-center mb-2">
-            <IconContext.Provider value={{ size: '2em', color: hoveredSubscription.color }}>
-              <hoveredSubscription.icon />
-            </IconContext.Provider>
-            <span className={`ml-2 text-lg ${ibmPlexMonoBold.className}`}>{hoveredSubscription.name}</span>
-          </div>
-          <p className={`${ibmPlexMonoRegular.className}`}>
-            ${hoveredSubscription.amount.toFixed(2)} / {hoveredSubscription.frequency}
-          </p>
-        </motion.div>
-      )}
     </div>
   )
 }
